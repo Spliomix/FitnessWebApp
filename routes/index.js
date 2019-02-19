dd8d7f77e81b00d640832386cefa3980f70e3d81
Glitch (fitness-webapp) <none>
2019-02-19 22:14:47 +0000

    👴⛷ add workout

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,12 @@ var User = require('../models/user');
 var passport = require('passport');
 var textdb = require('../models/text');
 var planDb = require('../models/plan');
+var workoutDb = require('../models/workout');
 var url = require('url');
 router.use(passport.session())
 var actURL;
 
-/* GET home page. */
-/*router.get('/', function (req, res, next) {
-    res.render('shop/index');
-});*/
+
 
 var mongoose = require('mongoose');
 var uri = process.env.DBURL;
@@ -68,8 +66,75 @@ router.get('/plan/:name', function(req, res) {
     });  
 });
 
+let actDateOwn = ()=>{//YYYYMMDD
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth()>=11?date.getMonth()+1:'0'+(date.getMonth()+1).toString();
+  let day = date.getDate()>=10?date.getDate():'0'+(date.getMonth()).toString();   
+  return year.toString()+month+day;
+}
+
+
+
+//render the typical workout mask with the corresponding plan
 router.get('/startWorkout', function(req, res){
-  res.render('shop/workout');
+
+  if(!req.isAuthenticated()){
+console.log("hombrine");
+           res.render('shop/index');
+     }else{
+       let planId = req.user.activePlan;
+    planDb.find({_id:planId},  function (err, plan) {
+      planDb.find({_id:planId},  function (err, workout) {
+          if(plan)
+            res.render('shop/workout', {plan: plan[0], workout:workout});
+          else
+            res.render('shop/workout');
+        });  
+    });  
+}
+  
+});
+
+
+
+//save the workout
+router.post('/saveWorkout', function(req, res){
+
+  let saveDate = actDateOwn();//YYYYMMDD
+  
+  let userId = req.user.id;
+  let planId = req.user.activePlan;
+  
+  let workout = [];//values from the exercises
+  let weights = req.body;
+  let weightPointer = 0 ;
+  
+    planDb.find({_id:planId},  function (err, plan) {
+      //calc the order of the weights
+      for(let i = 0; i<plan[0].exercises.length; ++i){
+        let exercises = [];
+            for(let j = 0; j<plan[0].exercises[i].reps.length; ++j){
+              exercises.push(weights[weightPointer++]);
+            } 
+        workout.push(exercises);
+        }
+       
+  let dataIn = {
+    plan: planId,
+    userId: userId,
+    exercises: workout, 
+    date: saveDate,
+  }
+  console.log(dataIn);
+    let data = new workoutDb(dataIn);//need a validation
+    data.save();
+      
+      res.sendStatus(200);
+    });  
+  
+ 
+  
 });
 
 module.exports = router;
